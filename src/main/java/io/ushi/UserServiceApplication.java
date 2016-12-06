d39d1d4d4099e2bcb629e929e9bd2d985b8754d4
zhouleibo <zhouleibo@qjdchina.com>
2016-12-06 22:45:32 +0800

    init

---
+++ src/main/java/io/ushi/UserServiceApplication.java
@@ -0,0 +1,24 @@
+package io.ushi;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class UserServiceApplication {
+
+    @Value("${config.name}")
+    String name = "World";
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello " + name;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
