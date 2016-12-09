417cd3cb566862117117580d7b6008dc909ea615
zhouleibo <zhouleibo@qjdchina.com>
2016-12-09 10:47:42 +0800

    use env instead of Value

--- src/main/java/io/ushi/UserServiceApplication.java
+++ src/main/java/io/ushi/UserServiceApplication.java
@@ -1,8 +1,10 @@
 package io.ushi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserServiceApplication {
 
+    @Autowired
+    Environment env;
+
     @Value("${config.name}")
     String name = "World";
 
     @RequestMapping("/")
     public String home() {
+        return "Hello " + env.getProperty("config.name");
+    }
+
+    @RequestMapping("/value")
+    public String value() {
         return "Hello " + name;
     }
 
